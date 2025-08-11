4646e7724062439207c706ea685128bae541dc29
sagar mahapatro <sagar.mahapatro@pearson.com>
2025-08-11 08:18:24 +0530

    fix target error in traning

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -7,9 +7,12 @@ df = pd.read_csv("data/raw/california_housing.csv")
 # Example preprocessing
 df = df[df["MedHouseVal"] < 5]  # Remove extreme values
 df["AveRooms"] = df["AveRooms"] / df["HouseAge"]
+df.rename(columns={"MedHouseVal": "target"}, inplace=True)
 
 # Ensure folder exists
 os.makedirs("data/processed", exist_ok=True)
+
+
 
 # Save processed data
 df.to_csv("data/processed/california_housing_processed.csv", index=False)
